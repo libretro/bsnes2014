28dbd6b5674aa95e0c41199e55ac96f76fef5da5
Alcaro <floating@muncher.se>
2015-12-21 16:54:35 +0100

    More cleanups.

--- target-libretro/libretro.cpp
+++ target-libretro/libretro.cpp
@@ -79,8 +79,6 @@ static void retro_log_default(enum retro_log_level level, const char *fmt, ...)
 }
 static retro_log_printf_t output;
 
-static const char * read_opt(const char * name, const char * defval);
-
 struct Callbacks : Emulator::Interface::Bind {
   retro_video_refresh_t pvideo_refresh;
   retro_audio_sample_batch_t paudio;
@@ -541,11 +539,11 @@ void retro_set_environment(retro_environment_t environ_cb)
    environ_cb(RETRO_ENVIRONMENT_SET_CONTROLLER_INFO, (void*)ports);
 }
 
-void retro_set_video_refresh(retro_video_refresh_t video_refresh) { core_bind.pvideo_refresh = video_refresh; }
-void retro_set_audio_sample(retro_audio_sample_t)    { }
-void retro_set_audio_sample_batch(retro_audio_sample_batch_t audio_sample) { core_bind.paudio  = audio_sample; }
-void retro_set_input_poll(retro_input_poll_t input_poll)          { core_bind.pinput_poll    = input_poll; }
-void retro_set_input_state(retro_input_state_t input_state)       { core_bind.pinput_state   = input_state; }
+void retro_set_video_refresh(retro_video_refresh_t cb)           { core_bind.pvideo_refresh = cb; }
+void retro_set_audio_sample(retro_audio_sample_t)                { }
+void retro_set_audio_sample_batch(retro_audio_sample_batch_t cb) { core_bind.paudio         = cb; }
+void retro_set_input_poll(retro_input_poll_t cb)                 { core_bind.pinput_poll    = cb; }
+void retro_set_input_state(retro_input_state_t cb)               { core_bind.pinput_state   = cb; }
 
 void retro_set_controller_port_device(unsigned port, unsigned device) {
   if (port < 2)
