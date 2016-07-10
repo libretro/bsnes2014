f8bd7b7eba15f05d040aa4a63eda80249a4647af
Alcaro <floating@muncher.se>
2016-07-10 17:20:04 +0200

    Fix the fix

--- sfc/alt/cpu/timing.cpp
+++ sfc/alt/cpu/timing.cpp
@@ -21,7 +21,7 @@ void CPU::last_cycle() {
     if (!status.frame_event_performed) {
       scheduler.exit(Scheduler::ExitReason::FrameEvent);
     }
-    status.frame_event_performed = true;
+    status.frame_event_performed = false;
 #endif
   }
 
